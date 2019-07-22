20ce8a41d3a659515eb513a3ae1352df7bb8d76f
aliffrhn <alif.farhanw@gmail.com>
2019-07-23 04:05:28 +0700

    add image slider, dummy image

--- Menu/Home/Home.js
+++ Menu/Home/Home.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 import Footers from '../Footers'
 
+import ImageSlider from 'react-native-image-slider'
+
 export default class Home extends Component{
 
+    constructor(){
+        super();
+        this.state = {
+            image : [
+                'http://placeimg.com/640/480/any',
+                'http://placeimg.com/640/480/any',
+                'http://placeimg.com/640/480/any'
+            ] 
+        }
+    }
     render(){
         
         return(
             <View style={{flex : 1}}>
                 <View style={{flex : 1}}>  
+                    <ImageSlider images={this.state.image} />
                     <Text>
                         Home
                     </Text>

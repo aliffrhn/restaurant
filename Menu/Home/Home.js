5c33067cb51a95ab3363bfe9c26a3e4e1a3e26f7
aliffrhn <alif.farhanw@gmail.com>
2019-07-23 15:08:45 +0700

    add card with api restaurant

--- Menu/Home/Home.js
+++ Menu/Home/Home.js
@@ -16,20 +16,31 @@ export default class Home extends Component{
                 'http://placeimg.com/640/480/any',
                 'http://placeimg.com/640/480/any'
             ],
-            categories : [] 
+            categories : [],
+            restaurant : []
         }
     }
 
-    getCategory = () =>{
+    // Function
+    getDataCategory = () =>{
         axios.get('https://developers.zomato.com/api/v2.1/categories', {headers: {'user-key' : '8585456090d478ea810a41f420f5dd5c'}})
         .then(res => {
             // console.log(res.data.categories)
             this.setState({categories : res.data.categories})
         })
     }
-
+    
+    getDataRestaurant = () =>{
+        axios.get('https://developers.zomato.com/api/v2.1/search?start=6&sort=rating', {headers: {'user-key' : '8585456090d478ea810a41f420f5dd5c'}})
+        .then(res => {
+            // console.log(res.data.restaurants)
+            this.setState({restaurant : res.data.restaurants})
+        })
+    }
+    
     componentDidMount(){
-        this.getCategory()
+        this.getDataCategory()
+        this.getDataRestaurant()
     }
 
     static navigationOptions = {
@@ -55,26 +66,43 @@ export default class Home extends Component{
                             )
                         })}
                     </Content>
-                    <Card>
-                        <CardItem>
-                            <Text>
-                                Title
-                            </Text>
-                        </CardItem>
-                        <CardItem>
-                            <Image style={{height : 200, width : null, flex : 1}} source={{uri : "http://placeimg.com/640/480/any"}} />
-                        </CardItem>
-                        <CardItem>
-                            <Left>
-                                <Ionicons name='md-star' size={30} color={'#f2994a'} />
-                            </Left>
-                            <Right>
-                                <Text>
-                                    Like
-                                </Text>
-                            </Right>
-                        </CardItem>
-                    </Card>
+                    {this.state.restaurant.map((data,key)=>{
+                        
+                        var image = ""
+                        if(data.restaurant.featured_image === ""){
+                            image = 'http://placeimg.com/640/480/any'
+                        }else{
+                            image = data.restaurant.featured_image
+                        }
+
+                        return(
+                            <Card key={key}>
+                                <CardItem>
+                                    <Text>
+                                        {data.restaurant.name}
+                                    </Text>
+                                </CardItem>
+                                <CardItem>
+                                    {console.log(data.restaurant)}
+                                    <Image style={{height : 200, width : null, flex : 1}} source={{uri : image}} />
+                                </CardItem>
+                                <CardItem>
+                                    <Left>
+                                        <Ionicons name='md-star' size={30} color={'#f2994a'} />
+                                        <Text>
+                                            {/* {console.log(data.restaurant.user_dating.aggregate_rating)} */}
+                                            {data.restaurant.user_rating.aggregate_rating}
+                                        </Text>
+                                    </Left>
+                                    <Right>
+                                        <Text>
+                                            Like
+                                        </Text>
+                                    </Right>
+                                </CardItem>
+                            </Card>
+                        ) 
+                    })}
                 </Content>
                 <Footers/>
             </View>
